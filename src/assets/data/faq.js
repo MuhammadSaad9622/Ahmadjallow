afd5a6cdf72d0afadf3c2e5aa660878e7df04ebc
MuhammadSaad9622 <saadnadeem962@gmail.com>
2024-03-16 00:23:28 +0500

    .

---
+++ src/assets/data/faq.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    title: "What is the purpose of the Impact-Driven Bye Gary NFT collection?",
+    text: "The Impact-Driven Bye Gary NFT collection aims to raise awareness and protest against perceived unfair treatment of the crypto community, particularly in response to remarks made by Gary Gensler. It serves as a collective voice to address obstacles hindering crypto growth and encourages community participation through signature petitions and NFT ownership.",
+  },
+  {
+    title: "How does the Bye Gary NFT collection plan to involve the crypto community?",
+    text: "The Bye Gary NFT collection involves the crypto community by offering 100k signature petitions as a sign of protest and resistance. Additionally, the collection is subdivided into 10 different community sections, each focusing on a different domain. Through a community-driven approach, existing NFT holders have the opportunity to expand the project through a DAO, allowing for further participation and influence in the initiative.",
+  },
+  {
+    title: "How does Bye Gary empower the crypto community in confronting regulatory authorities' unfair treatment?",
+    text: "Bye Gary provides a platform for individuals to unite and actively challenge the perceived unfair treatment of the crypto community by regulatory authorities. By participating in the collection, individuals can collectively voice their concerns and advocate for change.",
+  },
+  {
+    title: "What makes the NFTs in the Bye Gary collection unique in challenging Gary Gensler's stance?",
+    text: "Each NFT in the Bye Gary collection utilizes smart contracts to sign a petition, presenting a groundbreaking approach in the crypto space. This innovative use of NFT technology allows participants to directly challenge Gary Gensler's biased stance and advocate for fair treatment within the crypto community.",
+  },
+  {
+    title: "How does the DAO aspect of Bye Gary empower the community?",
+    text: "Bye Gary adopts a community-centered approach with its DAO (Decentralized Autonomous Organization) model, placing the community in the driving seat. Through this hybrid governance model, both Bye Gary NFT and Bye Gary Token ($BGT) holders have the ability to propose and vote on changes and improvements for the project. This empowers community members to actively participate in decision-making processes and shape the direction of the project according to their collective interests",
+  },
+ 
+];
+
+export default data;
---
+++ src/assets/data/footer/footerLinks.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    title: "Home",
+    url: "/",
+  },
+  {
+    title: "About",
+    url: "#about",
+  },
+  {
+    title: "Roadmap",
+    url: "#roadmap",
+  },
+ 
+];
+
+export default data;
